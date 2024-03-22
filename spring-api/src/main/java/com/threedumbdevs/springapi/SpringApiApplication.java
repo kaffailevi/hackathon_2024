97beda816bb5adf218a05747f9bc8a9bbc6fdac3
Kaffai Levente <levi.kaffai@gmail.com>
2024-03-22 14:17:19 +0200

    Blank spring-boot project initialized

---
+++ spring-api/src/main/java/com/threedumbdevs/springapi/SpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.threedumbdevs.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiApplication.class, args);
+    }
+
+}
---
+++ spring-api/src/test/java/com/threedumbdevs/springapi/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threedumbdevs.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
