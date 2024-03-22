0cb06b7a03f8c800928d569737864ca967d2f911
gallbenedekk <99737474+gallbenedekk@users.noreply.github.com>
2024-03-22 21:19:12 +0200

    Location and Post entities created

--- spring-api/src/main/java/com/threedumbdevs/springapi/entities/Pet.java
+++ spring-api/src/main/java/com/threedumbdevs/springapi/entities/Pet.java
@@ -7,6 +7,11 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -24,5 +29,31 @@ public class Pet {
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String breed_name;
+
+    @Column(nullable = false)
+    private Date birth_date;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "location")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Location lastLocation;
+
+    @ManyToMany
+    @JoinTable(
+            name = "pet_friends",
+            joinColumns = @JoinColumn(name = "pet_id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id")
+    )
+    private Set<Pet> friends = new HashSet<>();
+
+
+
+
 
 }
---
+++ spring-api/src/main/java/com/threedumbdevs/springapi/entities/Post.java
@@ -0,0 +1,34 @@
+package com.threedumbdevs.springapi.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = Post.TABLE_NAME)
+public class Post {
+    static final String TABLE_NAME = "Post";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+}
