d9203310073bc2962f4f5f1831f01723862a88c1
novakblanka03 <novakblanka2003@gmail.com>
2024-03-22 20:28:25 +0200

    Side menu finished

--- web-app/src/app/navbar/navbar.component.ts
+++ web-app/src/app/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {SideMenuComponent} from "../side-menu/side-menu.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    SideMenuComponent
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  isSideMenuOpen: boolean = false;
+  sideMenuDisplay: string = 'none';
+  showSideMenu() {
+    this.isSideMenuOpen = !this.isSideMenuOpen;
+    this.sideMenuDisplay = this.isSideMenuOpen ? 'block' : 'none';
+  }
 }
--- web-app/src/app/side-menu/side-menu.component.ts
+++ web-app/src/app/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-side-menu',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
-
+  @Input() isOpen: boolean = false;
 }
