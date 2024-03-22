d981ed0247d4b0f0cf9e06dc9c0f50d388f19418
gallbenedekk <99737474+gallbenedekk@users.noreply.github.com>
2024-03-22 18:41:34 +0200

    user entity created

---
+++ spring-api/src/main/java/com/threedumbdevs/springapi/entities/User.java
@@ -0,0 +1,31 @@
+package com.threedumbdevs.springapi.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = User.TABLE_NAME)
+public class User {
+
+    public static final String TABLE_NAME = "users";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    @Column(nullable = false)
+    private String last_name;
+
+    @Column(nullable = false)
+    private int age;
+
+
+}
