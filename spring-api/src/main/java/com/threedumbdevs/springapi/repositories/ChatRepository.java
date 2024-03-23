19b160dbbfeb3ced12f757c56ad36020de338876
gallbenedekk <99737474+gallbenedekk@users.noreply.github.com>
2024-03-23 03:38:37 +0200

    Chat service created

--- spring-api/src/main/java/com/threedumbdevs/springapi/repositories/ChatRepository.java
+++ spring-api/src/main/java/com/threedumbdevs/springapi/repositories/ChatRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ChatRepository extends CrudRepository<Chat, Long> {
-    //List<Chat> findAll();
+    List<Chat> findAll();
 
 }
