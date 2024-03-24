dc8252463d8c52db96f1b09ba7843bc1d5834cf5
Kaffai Levente <levi.kaffai@gmail.com>
2024-03-24 11:19:06 +0200

    Friend connections done

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -23,6 +23,15 @@ export class UserService {
     return this.httpClient.get<Chat[]>('http://localhost:8080/spring-api/api/v1/chat/chat_between/'+userId_1+'/'+userId_2);
   }
 
+
+  loadPets(userId:number) {
+    return this.httpClient.get<any>('http://localhost:8080/spring-api/api/v1/pet/user/'+userId);
+  }
+
+
+  addFriend(myPetId: number, id: 2) {
+    return this.httpClient.get<any>('http://localhost:8080/spring-api/api/v1/pet/addFriend/'+myPetId+'/'+id);
+  }
 }
 export interface Chat {
   id:number
